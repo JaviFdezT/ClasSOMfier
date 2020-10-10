9c984adce5c47e721128143893604839258e4cf3
Javier Fdez. Troncoso <javierfdeztroncoso@gmail.com>
2020-10-10 01:29:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#This script builds the code
+
+#deleted existing directories
 rm -rf build
 rm -rf CHANGES.txt
 rm -rf classifier*
@@ -6,12 +9,10 @@ rm -rf data/
 rm -rf dist/
 rm -rf nohup.ou
 
-rm -r /home/javi/system/anaconda/lib/python3.8/site-packages/classomf*
+#builds and installs
 python setup.py build bdist_wheel;pip install . 
 
-
-ls -l /home/javi/system/anaconda/lib/python3.8/site-packages/classomfier
-
+#runs test
 cd classomfier/test
 python -c "import classomfier; from classomfier import ClasSOMfier; nn=ClasSOMfier(6.43718,2,'dumpvoid.file');nn.execute()"
 cd -
